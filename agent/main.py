a0fb0d280e306947c863fd3a9e8b4bf386258eb6
ChristfriedBalizou <christfriedbalizou@gmail.com>
2020-06-02 21:39:08 +0200

    new: create a main cli agent file

---
+++ agent/main.py
@@ -0,0 +1,56 @@
+import os
+import enum
+import argparse
+
+from agent import handlers
+
+
+class AgentAction(enum.Enum):
+    """This class is a simple enumeration
+    of the possible action to perform by the agent
+    """
+
+    STATUS = "status"
+    START = "start"
+    STOP = "stop"
+    RESTART = "RESTART"
+
+
+AGENT_HANDLERS = {
+    AgentAction.STATUS: handlers.status,
+    AgentAction.STOP: handlers.stop,
+    AgentAction.START: handlers.start,
+    AgentAction.RESTART: handlers.restart,
+}
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.HelpFormatter,
+        description="""
+        Generate a Linux service and cron job to handle deployment
+        of a given repository
+        """
+    )
+
+    parser.add_argument(
+        "repository",
+        required=True,
+        help="The path to the repository directory to handle"
+    )
+
+    parser.add_argument(
+        "action",
+        choices=(
+            "status",
+            "start",
+            "stop",
+            "restart",
+        )
+    )
+
+    options = parser.parse_args()
+    action = AgentAction(options.action)
+    repository = os.path.normpath(options.repository)
+
+    AGENT_HANDLERS[action](repository)
