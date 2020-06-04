95baf1479041599b550415221732b4e018419f49
ChristfriedBalizou <christfriedbalizou@gmail.com>
2020-06-04 21:14:26 +0200

    new: implement a command class helper for subprocess

--- agent/handlers.py
+++ agent/handlers.py
@@ -1,3 +1,32 @@
+import shutil
+from subprocess import PIPE, Popen
+
+
+class Command:
+    """The command class is an Linux command line helper
+    base on subprocess which handle shell command output and
+    errors
+    """
+
+    def __init__(self, program: str):
+        """This function will make sure the program exist
+        or raise an error
+        """
+        self.program = shutil.which(program)
+        assert self.program, f"Can't find installation of: {program}"
+
+    def command(self, *args,  stdout=PIPE, stderr=PIPE, **kwargs):
+        """Using subprocess.Popen this function will execute
+        the given command line
+        """
+
+        communicate = kwargs.pop("communicate", {})
+        cmd = (self.program,) + args
+
+        with Popen(cmd, stdout=stdout, stderr=stderr, **kwargs) as proc:
+            return proc.communicate(**communicate)
+
+
 def status(repository):
     pass
 
